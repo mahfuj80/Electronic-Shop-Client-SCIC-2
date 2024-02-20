9dff990b5de3a935373d4990497b58c4ef87df07
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-02-21 00:20:45 +0600

    User layout and Routes created

---
+++ src/Layouts/UserLayout/UserLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const UserLayout = () => {
+  return (
+    <div className="">
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default UserLayout;
