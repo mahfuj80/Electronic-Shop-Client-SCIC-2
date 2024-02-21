a1bfaf89447a5c41735d8cfdcac88dbc68d5406b
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-02-21 21:26:58 +0600

    Contact Section Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from '../../components/Home/Banner/Banner';
+import Contact from '../../components/Home/Contact/Contact';
 import Faq from '../../components/Home/Faq/Faq';
 import FeaturedProducts from '../../components/Home/FeaturedProducts/FeaturedProducts';
 import NewsLatter from '../../components/Home/NewsLatter/NewsLatter';
@@ -11,6 +12,7 @@ const Home = () => {
       <FeaturedProducts />
       <OurProducts />
       <Faq />
+      <Contact />
       <NewsLatter />
     </div>
   );
