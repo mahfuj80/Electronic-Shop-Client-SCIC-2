d247158483335801219a5b049ed5f6c8d9127413
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-02-21 21:15:34 +0600

    Firebase installed

---
+++ src/Firebase/Firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAh4LSg0dLNnrgjgHRX3tP5_CL0e9ftlfQ',
+  authDomain: 'electronic-shop-scic2.firebaseapp.com',
+  projectId: 'electronic-shop-scic2',
+  storageBucket: 'electronic-shop-scic2.appspot.com',
+  messagingSenderId: '541575830421',
+  appId: '1:541575830421:web:b5bd48dbb003a1d4ac349c',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
