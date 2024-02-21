2d9c2aee64c74484c4f11750416d7b7cc62dbeb8
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-02-21 18:11:51 +0600

    Featured Products added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from '../../components/Home/Banner/Banner';
+import FeaturedProducts from '../../components/Home/FeaturedProducts/FeaturedProducts';
 import NewsLatter from '../../components/Home/NewsLatter/NewsLatter';
 
 const Home = () => {
   return (
     <div>
-      <Banner></Banner>
-      <NewsLatter></NewsLatter>
+      <Banner />
+      <FeaturedProducts />
+      <NewsLatter />
     </div>
   );
 };
